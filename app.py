16a48929bb25d9287e21df752d1b4044476b131c
Grettel09 <grettel.fernandez.arias@est.una.ac.cr>
2025-05-20 22:11:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # URLs de los archivos CSV (cambia TU_USUARIO por tu usuario de GitHub)
-clientes_url = "https://raw.githubusercontent.com/TU_USUARIO/Dashboard-ChocolateExport/main/clientes.csv"
-mercados_url = "https://raw.githubusercontent.com/TU_USUARIO/DashboardChocolate-Export/main/mercados.csv"
-exportaciones_url = "https://raw.githubusercontent.com/TU_USUARIO/DashboardChocolate-Export/main/exportaciones.csv"
-barreras_url = "https://raw.githubusercontent.com/TU_USUARIO/Dashboard-ChocolateExport/main/barreras.csv"
+clientes_url = "https://raw.githubusercontent.com/Grettel09/Dashboard-Chocolate-Export-/main/clientes.csv"
+mercados_url = "https://raw.githubusercontent.com/Grettel09/Dashboard-Chocolate-Export-/main/mercados.csv"
+exportaciones_url = "https://raw.githubusercontent.com/Grettel09/Dashboard-Chocolate-Export-/main/exportaciones.csv"
+barreras_url = "https://raw.githubusercontent.com/Grettel09/Dashboard-Chocolate-Export-/main/barreras.csv"
 
 # Cargar datos
 clientes = pd.read_csv(clientes_url)
